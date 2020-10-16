59cc2b9ce6cf97c38bb01053f22bdf09896cba8d
GooseWing <andryshik9@gmail.com>
2020-10-16 14:08:05 +0500

    Update background randomizer script

--- homepage-v5/js/script1.js
+++ homepage-v5/js/script1.js
@@ -1,3 +1,6 @@
-var images = ['../homepage-v5/media/city.jpg', '../homepage-v5/media/city1.jpg'];
+var images = ['../homepage-v5/media/city.jpg',
+              '../homepage-v5/media/city1.jpg',
+              '../homepage-v5/media/city2.jpg',
+              '../homepage-v5/media/city3.jpg'];
 document.getElementsByClassName('background')[0].style.background = 'url(' + images[Math.floor(Math.random() * images.length)] + ') no-repeat center fixed';
 document.getElementsByClassName('background')[0].style.backgroundSize = 'cover';
